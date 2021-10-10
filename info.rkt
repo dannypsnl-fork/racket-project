72bd1800a4e962b296a34790ba5ae8097a5f99c2
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-10 14:40:53 +0800

    [info] license

--- info.rkt
+++ info.rkt
@@ -5,4 +5,5 @@
 (define scribblings '(("scribblings/racket-project.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
+(define license '(Apache-2.0 OR MIT))
 (define pkg-authors '(dannypsnl))
