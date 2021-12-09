61ebfa8ea2f3c1a9a299f5a4997f3d9fa18526a2
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-09 23:26:49 +0800

    missing dependencies cover

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racket-project")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "cover"))
 (define scribblings '(("scribblings/racket-project.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
