38df8af20b36ddbd4318ed7d19f3a01546e22858
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-06-15 13:12:49 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -4,7 +4,7 @@ new_proj_name=$1
 sed -i "" "s/racket-project/$new_proj_name/g" ./info.rkt
 sed -i "" "s/racket-project/$new_proj_name/g" ./main.rkt
 sed -i "" "s/racket-project/$new_proj_name/g" ./scribblings/racket-project.scrbl
-echo "# $new_proj_name" > README.md
+sed -i "" "s/racket-project/$new_proj_name/g" ./README.md
 
 # author name
 if [ -z $2 ]; then echo "keep origin github id"; else sed -i "" "s/dannypsnl/$2/g" ./info.rkt; fi
